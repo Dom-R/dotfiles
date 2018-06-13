ad7c2c916aa6fee5c8adc634e4bc43cd3d7cb24f
Dom-R <Dom-R@users.noreply.github.com>
2018-06-13 07:13:54 +0000

    added setup for git username and email

--- git/setup.sh
+++ git/setup.sh
@@ -2,6 +2,19 @@
 
 . scripts/functions.sh
 
+function configure_git() {
+      username="Dom-R"
+      email="Dom-R@users.noreply.github.com"
+
+      info "Configuring git..."
+      if git config --global user.name "$username" && \
+         git config --global user.email "$email"; then
+          success "Git configuration succeeded."
+      else
+          error "Git configuration failed."
+      fi
+}
+
 function git_aliases() {
     git config --global alias.co checkout
     git config --global alias.br branch
@@ -9,6 +22,7 @@ function git_aliases() {
     git config --global alias.st status
 }
 
+configure_git
 info "Adding git aliases"
 if git_aliases; then
   success "Adding git aliases succeded"
