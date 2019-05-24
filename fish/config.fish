0653d502b06c5de3cfe3aafa5c971f212b8ecacc
Dom-R <Dom-R@users.noreply.github.com>
2019-05-23 21:46:38 -0300

    moving from the silver searcher to ripgrep

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
 # rbenv compatibility
 status --is-interactive; and source (rbenv init -|psub)
 
-set --export FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+set --export FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
