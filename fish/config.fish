fade9a8711e299c5b5771c404e2a3c2a14f4f04d
Dominik Reller <Dom-R@users.noreply.github.com>
2019-07-03 14:28:29 -0300

    added fix for locale errors on nvim initialization

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
 # rbenv compatibility
 status --is-interactive; and source (rbenv init -|psub)
 
+# force fzf to use ripgrep
 set --export FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+
+# Set locale due to nvim error message related to locale. More here https://stackoverflow.com/questions/56716993
+set --export LC_ALL 'en_US.UTF-8'
