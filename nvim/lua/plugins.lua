b265a5e22b94a4311c04a0ea441d5cb4f9281276
Dominik Reller <Dom-R@users.noreply.github.com>
2021-06-24 09:45:43 +0000

    add automatic nvim packer installation when not available

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,15 @@
+-- install packer if not installed
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+-- helpers
 local g = vim.g      -- a table to access global variables
 
 local function map(mode, lhs, rhs, opts)
