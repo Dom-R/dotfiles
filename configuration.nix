150b14e61a1b00d51f7bc41b693266c414479f92
Dominik Reller <Dom-R@users.noreply.github.com>
2022-10-20 13:22:42 +0200

    add blueman to manage bluetooth on nixos

--- configuration.nix
+++ configuration.nix
@@ -92,6 +92,7 @@
 
   # Enable bluetooth
   hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
