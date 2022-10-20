ff703ac5e16f588c3a29b933d820a4c8d39d830f
Dominik Reller <Dom-R@users.noreply.github.com>
2022-10-20 13:26:53 +0200

    remove treesitter context in favour of lualine implementation

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -90,11 +90,46 @@ return require('packer').startup(function()
 
   -- status line
   use 'nvim-lualine/lualine.nvim'
+  local breadcrump_sep = " ⟩ "
   require('lualine').setup {
     options = {
       section_separators = '',
       component_separators = '',
       icons_enabled = false
+    },
+    sections = {
+        lualine_a = {"mode"},
+        lualine_b = {
+          {
+              "filename",
+              path = 1,
+              separator = vim.trim(breadcrump_sep),
+              fmt = function(str)
+                local path_separator = package.config:sub(1, 1)
+                return str:gsub(path_separator, breadcrump_sep)
+              end
+          },
+          { "aerial", sep = breadcrump_sep },
+        },
+        lualine_c = {},
+        lualine_x = {"lsp_progress"},
+        lualine_y = {
+          "diagnostics",
+          {
+              "encoding",
+              cond = function()
+                -- UTF-8 is the de-facto standard encoding and is what
+                -- most users expect by default. There's no need to
+                -- show encoding unless it's something else.
+                local fenc = vim.opt.fenc:get()
+                return string.len(fenc) > 0 and string.lower(fenc) ~= "utf-8"
+              end,
+          },
+          "filetype",
+          "fileformat",
+          "progress",
+        },
+        lualine_z = {"location"},
     }
   }
 
@@ -142,7 +177,6 @@ return require('packer').startup(function()
       additional_vim_regex_highlighting = false
     }
   }
-  use 'nvim-treesitter/nvim-treesitter-context'
 
   -- automatic annotations/comments with :Neogen
   use {
