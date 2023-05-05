7675e8a49aa0991d623ab67f515430a23c55fef8
Dominik Reller <Dom-R@users.noreply.github.com>
2023-05-05 12:35:59 +0200

    improve noice configuration

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -28,6 +28,14 @@
     event = "VeryLazy",
     config = function()
       require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
         cmdline = {
           format = {
             cmdline = { icon = ":" },
@@ -42,11 +50,9 @@
           view_search = "mini", -- view for search count messages. Set to `false` to disable
         },
         routes = {
-          {
-            view = "mini",
-            filter = { event = "msg_show", find = ".*written" },
-            --opts = { skip = true },
-          },
+          { view = "mini", filter = { event = "msg_show", find = ".*written" }, },
+          --{ view = "mini", filter = { event = "msg_show", find = "Pattern not found" }, },
+          { filter = { event = "msg_show", find = "search hit" }, skip = true },
         },
       })
     end,
