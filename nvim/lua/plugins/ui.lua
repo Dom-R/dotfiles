e9870655cb8b7f4d196a382b9961286b34e6eb8c
Dominik Reller <Dom-R@users.noreply.github.com>
2023-09-19 12:21:44 +0200

    remove nvim filetree

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,4 +1,12 @@
 return {
+  -- undo tree
+  {
+    'mbbill/undotree',
+    keys = {
+      { '<leader>u', vim.cmd.UndotreeToggle }
+    }
+  },
+
   -- indentation visualizer
   {
     'lukas-reineke/indent-blankline.nvim',
@@ -24,36 +32,43 @@ return {
   },
 
   -- add actions animation (ex: movement, resizing, etc)
+  --{
+  --  'echasnovski/mini.animate',
+  --  config = function()
+  --    require('mini.animate').setup()
+  --  end
+  --},
+
   {
-    'echasnovski/mini.animate',
+    'kevinhwang91/nvim-ufo',
     config = function()
-      require('mini.animate').setup()
-    end
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true
+      }
+      local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+        require('lspconfig')[ls].setup({
+          capabilities = capabilities
+          -- you can add other fields for setting up lsp server in this table
+        })
+      end
+      require('ufo').setup()
+
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+    end,
+    dependencies = {
+      'kevinhwang91/promise-async'
+    }
   },
 
- {
-   'kevinhwang91/nvim-ufo',
-   config = function()
-     local capabilities = vim.lsp.protocol.make_client_capabilities()
-     capabilities.textDocument.foldingRange = {
-       dynamicRegistration = false,
-       lineFoldingOnly = true
-     }
-     local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-     for _, ls in ipairs(language_servers) do
-       require('lspconfig')[ls].setup({
-         capabilities = capabilities
-         -- you can add other fields for setting up lsp server in this table
-       })
-     end
-     require('ufo').setup()
-
-     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-     vim.o.foldlevelstart = 99
-     vim.o.foldenable = true
-   end,
-   dependencies = {
-     'kevinhwang91/promise-async'
-   }
- },
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      require("oil").setup()
+    end
+  },
 }
