c91dd36ab0d8ee121f428ec02bad12a156547d2d
Dominik Reller <Dom-R@users.noreply.github.com>
2023-11-07 13:22:07 +0100

    disable null-ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -33,38 +33,37 @@ return {
   },
 
   -- linter
-  {
-    'jose-elias-alvarez/null-ls.nvim',
-    dependencies = { {'nvim-lua/plenary.nvim'} },
-    config = function()
-      local null_ls = require("null-ls")
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  --{
+  --  'jose-elias-alvarez/null-ls.nvim',
+  --  dependencies = { {'nvim-lua/plenary.nvim'} },
+  --  config = function()
+  --    local null_ls = require("null-ls")
+  --    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.diagnostics.typos,
-          null_ls.builtins.diagnostics.rubocop,
-          null_ls.builtins.formatting.trim_whitespace,
-        },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({
-                  filter = function(client)
-                    -- By default, ignore any formatters provider by other LSPs
-                    return client.name == "null-ls"
-                  end,
-                  bufnr = bufnr,
-                })
-              end,
-            })
-          end
-        end,
-      })
-    end
-  },
+  --    null_ls.setup({
+  --      sources = {
+  --        null_ls.builtins.diagnostics.typos,
+  --        null_ls.builtins.diagnostics.rubocop,
+  --      },
+  --      on_attach = function(client, bufnr)
+  --        if client.supports_method("textDocument/formatting") then
+  --          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  --          vim.api.nvim_create_autocmd("BufWritePre", {
+  --            group = augroup,
+  --            buffer = bufnr,
+  --            callback = function()
+  --              vim.lsp.buf.format({
+  --                filter = function(client)
+  --                  -- By default, ignore any formatters provider by other LSPs
+  --                  return client.name == "null-ls"
+  --                end,
+  --                bufnr = bufnr,
+  --              })
+  --            end,
+  --          })
+  --        end
+  --      end,
+  --    })
+  --  end
+  --},
 }
