cc3b9d41f5d5bb1a610f1bc2ad78df2d34e037ea
Dominik Reller <Dom-R@users.noreply.github.com>
2023-11-07 13:24:00 +0100

    add new formatter through conform.nvim

---
+++ nvim/lua/plugins/formatter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'stevearc/conform.nvim',
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+      formatters_by_ft = {
+        ["*"] = { "trim_whitespace", "trim_newlines" }
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  }
+}
