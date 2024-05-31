3b017b0fcdd311c0354f53979a8e296698dad86c
Dominik Reller <Dom-R@users.noreply.github.com>
2024-05-31 10:27:54 +0200

    remove vim fugitive

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,10 +1,11 @@
 return {
   {
-    'tpope/vim-fugitive',
-    dependencies = { {'tpope/vim-rhubarb'} },
-    cmd = {
-      "Git",
-      "GBrowse"
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
     },
   }
 }
