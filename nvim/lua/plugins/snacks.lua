5e68e9b414088d74b18176e2c21a07d09ef0bd13
Dominik Reller <Dom-R@users.noreply.github.com>
2025-07-07 16:26:43 +0200

    change git browse to snacks plugins

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      gitbrowse = {
+        -- your gitbrowse configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      --indent = {
+      --  enabled = true,
+      --  char = "a",
+      --  only_scope = true,
+      --  only_current = false
+      --  -- your indent configuration comes here
+      --  -- or leave it empty to use the default settings
+      --  -- refer to the configuration section below
+      --}
+    },
+    custom_keys = {
+      {
+        "<leader>oy",
+        function()
+          Snacks.gitbrowse.open()
+        end,
+        mode = { "n", "v" },
+        desc = "Open git file in browser"
+      },
+    },
+  }
+}
