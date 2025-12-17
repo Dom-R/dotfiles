1a9548cb64c781290e4683c732559b04e5ebeddc
Dominik Reller <Dom-R@users.noreply.github.com>
2025-12-17 15:36:15 +0100

    change colorscheme to tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,58 +1,40 @@
 return {
   {
-    'marko-cerovac/material.nvim',
+    "folke/tokyonight.nvim",
+    lazy = false,
     priority = 1000,
-    init = function ()
-      require('material').setup({
-        plugins = { -- Uncomment the plugins that you use to highlight them
-          "blink",
-          -- "coc",
-          -- "colorful-winsep",
-          -- "dap",
-          -- "dashboard",
-          -- "eyeliner",
-          -- "fidget",
-          -- "flash",
-          -- "gitsigns",
-          -- "harpoon",
-          -- "hop",
-          "illuminate",
-          "indent-blankline",
-          -- "lspsaga",
-          -- "mini",
-          -- "neo-tree",
-          -- "neogit",
-          -- "neorg",
-          -- "neotest",
-          "noice",
-          -- "nvim-cmp",
-          -- "nvim-navic",
-          "nvim-notify",
-          -- "nvim-tree",
-          -- "nvim-web-devicons",
-          -- "rainbow-delimiters",
-          -- "sneak",
-          "telescope",
-          "trouble",
-          -- "which-key",
+    opts = {},
+    init = function()
+      require("tokyonight").setup({
+        -- use the night style
+        style = "night",
+        -- transparent background
+        transparent = true,
+        -- disable italic at keywords
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
         },
-
-        disable = {
-          colored_cursor = false, -- Disable the colored cursor
-          borders = false, -- Disable borders between verticaly split windows
-          background = true, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-          term_colors = false, -- Prevent the theme from setting terminal colors
-          eob_lines = false -- Hide the end-of-buffer lines
-        },
-
-        high_visibility = {
-          lighter = false, -- Enable higher contrast text for lighter style
-          darker = true -- Enable higher contrast text for darker style
-        },
-        lualine_style = "stealth", -- Lualine style ( can be 'stealth' or 'default' )
+        on_highlights = function(hl, c)
+          hl.Type = {
+            fg = "#c792ea"
+          }
+          hl["@keyword"] = {
+            fg = "#89ddff"
+          }
+          hl.LineNr = {
+            fg = "#515151"
+          }
+          hl.LineNrAbove = {
+            fg = "#515151"
+          }
+          hl.LineNrBelow = {
+            fg = "#515151"
+          }
+        end
       })
-      vim.g.material_style = "darker"
-      vim.cmd 'colorscheme material'
+
+      vim.cmd[[colorscheme tokyonight]]
     end
   }
 }
